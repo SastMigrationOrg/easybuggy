e46fb8dc976ddcfb8d31f65c227e3a982b86b432
migration-sast <136586733+migration-sast@users.noreply.github.com>
2023-06-21 13:11:41 +0200

    Update Test.java

--- src/main/java/org/t246osslab/easybuggy/Test.java
+++ src/main/java/org/t246osslab/easybuggy/Test.java
@@ -3,6 +3,5 @@ public class Test {
    //TODO should be random generated when starting the server
     String PASSWORD = "!!webgoat_admin_1234!!";
    String PASSWORD1 = "!!webgoat_admin_1234!!1";
-    String ADMIN_PASSWORD_LINK = "375afe1104f4a487a73823c50a9292a2"; 
-   
+    String ADMIN_PASSWORD_LINK = "375afe1104f4a487a73823c50a9292a2";   
 }
